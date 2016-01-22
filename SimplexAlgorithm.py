e3a3ad6f045fe0149e1e075ae49646004144ff18
milleraj66 <milleraj66@gmail.com>
2016-01-22 11:35:17 -0800

    Works for example given in class TODO: Add user input TODO: Exeption handling TODO: Cleanup output matrix TODO: Testing->Including a couple bugs Dillion and I found TODO: See TODO's in SimplexAlgorithm.py

--- SimplexAlgorithm.py
+++ SimplexAlgorithm.py
@@ -1,16 +1,23 @@
+# coding=utf-8
 '''
 Author:     Arthur J. Miller
 Date:       01-20-2016
+            UPDATE: AJM 01-22-2016
 
-Overview:
-        # Must make objective row negative for X1,X2,Etc
-        # RowSize = (ActualRowSize -1)
-        # ColSize = (ActualColSize -1)
+PURPOSE:
+        You are required to develop a user friendly C++/Java/C#  code that will implement naïve simplex algorithm
+        Your code should be able to handle 2, 3 and 4 variable maximization LP problems.
+        Your code should also print the contents of the simplex tableau after each iteration.
+PROJECT NOTES
+        # Must make objective row (ObjFunctArray) of augmented matrix (SimplexArray) negative for X1,X2,Etc
+        # RowSize = (Actual Row Size -1)
+        # ColSize = (Actual Col Size -1)
         # Must change vales to float to handle normalization step
-
-        # TODO Exeption handling for 2Di
+ACTIVE TODO
+        # TODO Exception handling for 2Di
         # TODO 2Dii Need a way to check for negative values in ratio array... algorithm doesnt use negative values
         # TODO how many times do we loop the whole algorithm? Are there other checks for NO MAX VALUE
+        # TODO Exeption handling for user input EX. variable size and constraint size
 
 Paramaters:
 '''
@@ -38,55 +45,55 @@ def main():
     #       b. Create initial Augmented array using constraint equations and objective equation
     SimplexArray = [Const1Array,Const2Array,ObjFunctArray]
     print(SimplexArray)
-    #       c. Scan bottom row for smallest value -> use that column as entering column
-    PivotColVal = min(SimplexArray[ColSize])
-    print("PivotColVal: ", PivotColVal)
-    PivotCol = SimplexArray[ColSize].index(PivotColVal)
-    print("PivotCol: ", PivotCol)
-    #       d. Find minimum value of ratios of {last column value : enter column value} -> this is pivot
-    RatioArray = []
-    for i in range(ColSize): #Dont do ratio for Objective Fucntion Row
-        RatioArray.append(SimplexArray[i][RowSize]/SimplexArray[i][PivotCol])
-        print("Ratio Array: ", RatioArray)
-    #           i. if all entries in columns are 0 or negative -> no max solution
-    NoMaxSol = all(v == 0 for v in RatioArray)
-    print("NoMaxSol: ", NoMaxSol)
-    if NoMaxSol == 1:
-        print("No Max Solution... All ratio values are zero")
-        return 1
-    #           ii. Unimplmented. if tie at minimum ratio -> choose either
-    PivotRowVal = min(RatioArray)
-    print("PivotRowVal: ", PivotRowVal)
-    PivotRow = RatioArray.index(PivotRowVal)
-    print("PivotRow: ",PivotRow)
-    #       e. Normalize pivot value by checking if 1, then dividing each row value by itself
-    print(SimplexArray[PivotRow])
-    PivotVal = float(SimplexArray[PivotRow][PivotCol])
-    if SimplexArray[PivotRow][PivotCol] != 1:
-        for i in range(RowSize+1):
-            SimplexArray[PivotRow][i] = SimplexArray[PivotRow][i]/PivotVal
+    while (1):
+        #       c. Scan bottom row for smallest value -> use that column as entering column
+        PivotColVal = min(SimplexArray[ColSize])
+        print("PivotColVal: ", PivotColVal)
+        PivotCol = SimplexArray[ColSize].index(PivotColVal)
+        print("PivotCol: ", PivotCol)
+        #       d. Find minimum value of ratios of {last column value : enter column value} -> this is pivot
+        RatioArray = []
+        for i in range(ColSize): #Dont do ratio for Objective Fucntion Row
+            RatioArray.append(SimplexArray[i][RowSize]/SimplexArray[i][PivotCol])
+            print("Ratio Array: ", RatioArray)
+        #           i. if all entries in columns are 0 or negative -> no max solution
+        NoMaxSol = all(v == 0 for v in RatioArray)
+        print("NoMaxSol: ", NoMaxSol)
+        if NoMaxSol == 1:
+            print("No Max Solution... All ratio values are zero")
+            return 1
+        #           ii. Unimplmented. if tie at minimum ratio -> choose either THIS IS HANDLED BY THE MIN FUCTION
+        PivotRowVal = min(RatioArray)
+        print("PivotRowVal: ", PivotRowVal)
+        PivotRow = RatioArray.index(PivotRowVal)
+        print("PivotRow: ",PivotRow)
+        #       e. Normalize pivot value by checking if 1, then dividing each row value by itself
         print(SimplexArray[PivotRow])
+        PivotVal = float(SimplexArray[PivotRow][PivotCol])
+        if SimplexArray[PivotRow][PivotCol] != 1:
+            for i in range(RowSize+1):
+                SimplexArray[PivotRow][i] = SimplexArray[PivotRow][i]/PivotVal
+            print(SimplexArray[PivotRow])
 
-    #       f. Make every other value in pivot column == 0 by, elementary row operations
-    #           i. R_i' <= R_i - PivotVal*R(Pivot)
-    print("SimplexArray Before Row Ops: ", SimplexArray)
-    for i in range(ColSize+1):
-        TempRowOpVal = SimplexArray[i][PivotCol]        #The value SimplexArray[i][PivotCol] will become zero before operation done
-        for j in range(RowSize+1):
-            if(i == PivotRow):
+        #       f. Make every other value in pivot column == 0 by, elementary row operations
+        #           i. R_i' <= R_i - PivotVal*R(Pivot)
+        print("SimplexArray Before Row Ops: ", SimplexArray)
+        for i in range(ColSize+1):
+            TempRowOpVal = SimplexArray[i][PivotCol]        #The value SimplexArray[i][PivotCol] will become zero before operation done
+            for j in range(RowSize+1):
+                if(i == PivotRow):
+                    break
+                SimplexArray[i][j] = SimplexArray[i][j] - TempRowOpVal*SimplexArray[PivotRow][j]
+        print("SimplexArray After Row Ops: ",SimplexArray)
+        #       g. Check bottom row, if no values are negative DONE! bottom right value is Z maximum
+        for i in range(RowSize+1):
+            if(SimplexArray[ColSize][i] < 0):
                 break
-            SimplexArray[i][j] = SimplexArray[i][j] - TempRowOpVal*SimplexArray[PivotRow][j]
-    print("SimplexArray After Row Ops: ",SimplexArray)
-    #       g. Check bottom row, if no values are negative DONE! bottom right value is Z maximum
-    for i in range(RowSize+1):
-        if(SimplexArray[RowSize][i] < 0):
-            break
-        else:
-            print("Bottom Row Shows Maximum Solution: ",SimplexArray)
-            return 0
-
-    #       h. print Table
-    print(SimplexArray)
+            else:
+                print("Bottom Row Shows Maximum Solution: ",SimplexArray)
+                return 0
+        #       h. print Table
+        print(SimplexArray)
     # Function to print table
 '''    def printMatrix(testMatrix):
             print ' ',
