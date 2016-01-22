c15ef781bedf575111016c700baa878aedaf2880
milleraj66 <milleraj66@gmail.com>
2016-01-22 12:18:42 -0800

    ADDED: Fixed logic error in the code that checks if last row has any negative values in it.
    Works for 1st example given in class
    Does not work for 2nd example given on BB
    
    TODO: Add user input
    TODO: Exeption handling
    TODO: Cleanup output matrix
    TODO: Testing->Including a couple bugs Dillion and I found
    TODO: See TODO's in SimplexAlgorithm.py

--- SimplexAlgorithm.py
+++ SimplexAlgorithm.py
@@ -14,18 +14,18 @@ PROJECT NOTES
         # ColSize = (Actual Col Size -1)
         # Must change vales to float to handle normalization step
 ACTIVE TODO
+        # TODO WONT WORK WITH SECOND EXAMPLE CASE -> BUG
         # TODO Exception handling for 2Di
         # TODO 2Dii Need a way to check for negative values in ratio array... algorithm doesnt use negative values
         # TODO how many times do we loop the whole algorithm? Are there other checks for NO MAX VALUE
         # TODO Exeption handling for user input EX. variable size and constraint size
+        # TODO Print Output matrix in a nice format
 
-Paramaters:
 '''
 
 #!/usr/bin/python
 
 # import modules used here -- sys is a very standard one
-#nonpy
 
 # Gather our code in a main() function
 def main():
@@ -34,17 +34,27 @@ def main():
     #ObjFunctSize = 2
     RowSize = 4         # 0 to 4
     ColSize = 2         # 0 to 2
+    '''RowSize = 5         # 0 to 5
+    ColSize = 3         # 0 to 3'''
     #           i. Enter obj funt values -> insert into array
     ObjFunctArray =  [float(-40),float(-50),float(0),float(0),float(0)]
+    '''ObjFunctArray =  [float(-4),float(-6),float(0),float(0),float(0),float(0)]'''
+
     #       b. Number of constraints
     #           i. Enter constraint values (in inequality form) -> insert into ixj 2d array
     Const1Array = [float(1),float(2),float(1),float(0),float(40)] #40
     Const2Array = [float(4),float(3),float(0),float(1),float(120)] #120
+    '''Const1Array = [float(-1),float(1),float(1),float(0),float(0),float(11)]
+    Const2Array = [float(1),float(1),float(0),float(1),float(0),float(27)]
+    Const3Array = [float(2),float(5),float(0),float(0),float(0),float(90)]'''
     #### 2. Start Algorithm
     #       a. Convert inequality constraints to equation using slack variables
     #       b. Create initial Augmented array using constraint equations and objective equation
     SimplexArray = [Const1Array,Const2Array,ObjFunctArray]
+    '''SimplexArray = [Const1Array,Const2Array,Const3Array,ObjFunctArray]'''
+
     print(SimplexArray)
+    # repeat algorithn until RETURN 0 or RETURN 1
     while (1):
         #       c. Scan bottom row for smallest value -> use that column as entering column
         PivotColVal = min(SimplexArray[ColSize])
@@ -52,6 +62,7 @@ def main():
         PivotCol = SimplexArray[ColSize].index(PivotColVal)
         print("PivotCol: ", PivotCol)
         #       d. Find minimum value of ratios of {last column value : enter column value} -> this is pivot
+        # Form an array that holds the ratio values
         RatioArray = []
         for i in range(ColSize): #Dont do ratio for Objective Fucntion Row
             RatioArray.append(SimplexArray[i][RowSize]/SimplexArray[i][PivotCol])
@@ -63,6 +74,7 @@ def main():
             print("No Max Solution... All ratio values are zero")
             return 1
         #           ii. Unimplmented. if tie at minimum ratio -> choose either THIS IS HANDLED BY THE MIN FUCTION
+        # Find minimum value in array THIS DOES NOT REMOVE NEGATIVE VALUES
         PivotRowVal = min(RatioArray)
         print("PivotRowVal: ", PivotRowVal)
         PivotRow = RatioArray.index(PivotRowVal)
@@ -86,14 +98,22 @@ def main():
                 SimplexArray[i][j] = SimplexArray[i][j] - TempRowOpVal*SimplexArray[PivotRow][j]
         print("SimplexArray After Row Ops: ",SimplexArray)
         #       g. Check bottom row, if no values are negative DONE! bottom right value is Z maximum
+        counter = 0
         for i in range(RowSize+1):
             if(SimplexArray[ColSize][i] < 0):
-                break
-            else:
-                print("Bottom Row Shows Maximum Solution: ",SimplexArray)
-                return 0
+                counter = counter+1
+        if (counter == 0):
+            print("Bottom Row Shows Maximum Solution: ",SimplexArray)
+            return 0
         #       h. print Table
         print(SimplexArray)
+
+# Standard boilerplate to call the main() function to begin
+# the program.
+if __name__ == '__main__':
+  main()
+
+
     # Function to print table
 '''    def printMatrix(testMatrix):
             print ' ',
@@ -104,8 +124,3 @@ def main():
                   print i, ' '.join(element)
 SimplexAlgorithm().main()
 '''
-
-# Standard boilerplate to call the main() function to begin
-# the program.
-if __name__ == '__main__':
-  main()
